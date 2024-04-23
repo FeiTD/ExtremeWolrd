c258072c07a34ef7b2c98c7ba55338b1dc3a4200
费天栋 <2946016190@qq.com>
2024-04-23 23:46:21 +0800

    Push

---
+++ Src/Client/Assets/TestNet.cs
@@ -0,0 +1,18 @@
+﻿using Network;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NetClient.Instance.Init("127.0.0.1", 8000);
+		NetClient.Instance.Connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
